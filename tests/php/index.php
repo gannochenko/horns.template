ef9e505a59f1583d6690a304391eaf2a4ea0eb4d
Sergey <awesome1888@gmail.com>
2016-07-11 19:46:28 +0200

    js and php are equal

--- tests/php/index.php
+++ tests/php/index.php
@@ -1,6 +1,11 @@
-<?
-include "horns.php";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Horns tests</title>
+</head>
+<body>
+
 	<style>
 		.green{
 			color: green;
@@ -22,7 +27,11 @@ include "horns.php";
 		}
 	</style>
 
+	<h1>Horns test page: PHP</h1>
+
 <?
+include "horns.php";
+
 $helpers = array(
 
 	'quote' => function($arg){
@@ -109,25 +118,120 @@ $tests = array(
 	),
 	array(
 		'data' => [
-			'jumps' => [
+			'guestName' => 'Steve',
+			'meal' => [
 				[
-					'personA' => 'brown fox',
-					'personB' => 'dog',
+					'title' => 'breakfast',
+					'time' => '9:00 am',
+					'menu' => ['Cup of coffee', 'Sandwich', 'Cigarette']
 				],
 				[
-					'personA' => 'crazy elk',
-					'personB' => 'brutal crocodile',
+					'title' => 'dinner',
+					'time' => '1:00 pm',
+					'menu' => ['Vegetable salad', 'Mushroom soup', 'Cherry compote', 'Bread']
 				],
 				[
-					'personA' => 'smelly squirrel',
-					'personB' => 'gorgeous giraffe',
+					'title' => 'supper',
+					'time' => '6:00 pm',
+					'menu' => ['Sausages with roasted potatoes', 'Cup of tea', 'Apple pie', 'Good movie to watch']
 				]
+			],
+		],
+		'template' => '<h3>Dear {{guestName}}, your menu for today is:</h3>
+			<ul>
+			{{#meal}}
+				<li>
+					<b>{{ucFirst title}}</b><br />
+					Time: {{time}}<br />
+					Dishes: {{implode menu}}
+				</li>
+			{{/meal}}
+			</ul>',
+		'name' => '',
+	),
+	array(
+		'data' => [
+			'basket'=> [
+				[
+					'name'=> 'Fish&Chips',
+					'quantity'=> 3,
+					'price'=> 10
+				],
+				[
+					'name'=> 'Pair of socks',
+					'quantity'=> 5,
+					'price'=> 700
+				],
+				[
+					'name'=> 'Microwave owen',
+					'quantity'=> 1,
+					'price'=> 1500
+				],
+				[
+					'name'=> 'Mercedes 600',
+					'quantity'=> 1,
+					'price'=> 900000000,
+					'discount'=> 0.03
+				]
+			],
+			'payment'=> [
+				['title'=> 'Visa/Master Card', 'value'=> 'card'],
+				['title'=> 'Cash', 'value'=> 'cash', 'default'=> true]
 			]
 		],
-		'template' => '<h3>Jumping around:</h3> {{#jumps}} {{> jumping}} <br /> {{/jumps}}',
-		'name' => 'massive_jump',
+		'template' => '<form>
+				<h3>Your order is:</h3>
+				<div class="container">
+					{{#if isNotEmpty basket}}
+						<table cellpadding="0" cellspacing="0">
+							<thead><tr><th>Name</th><th>Price</th><th>Quantity</th><th>Cost</th></tr></thead>
+							<tbody>
+							{{#basket}}
+							<tr>
+								<td>{{name}}</td>
+								<td>${{price}}</td>
+								<td>{{quantity}}</td>
+								<td>{{#if discount}}<s>{{/if}}${{getCost this}}{{#if discount}}</s> (with discount: ${{getDiscountCost this}}){{/if}}</td>
+								</tr>
+							{{/basket}}
+							</tbody>
+						</table>
+					{{else}}
+						<span class="red">Your basket is empty</span>
+					{{/if}}
+				</div>
+				Total price: <span class="green">${{totalPrice basket}}</span><br /><br />
+				Pay with:
+				<select>
+					{{#payment}}
+					<option value="{{value}}" {{#if default}} selected="selected"{{/if}}>{{title}}</option>
+					{{/payment}}
+				</select>
+			</form>',
+		'name' => '',
 	),
-	/*
+	array(
+		'data' => [
+			'basket'=> [
+				[
+					'name'=> 'Fish&Chips',
+					'quantity'=> 3,
+					'price'=> 10
+				],
+				[
+					'name'=> 'Pair of socks',
+					'quantity'=> 5,
+					'price'=> 700
+				],
+			],
+			'payment'=> [
+				['title'=> 'Visa/Master Card', 'value'=> 'card'],
+				['title'=> 'Cash', 'value'=> 'cash', 'default'=> true]
+			]
+		],
+		'template' => '<la>{{getCostAlt basket.0 this}}</la>',
+		'name' => '',
+	),
 	array(
 		'data' => [
 			'weapon' => [
@@ -173,114 +277,44 @@ $tests = array(
 		'name' => '',
 	),
 	array(
-		'data' => [],
-		'template' => '<h3>Dear {{guestName}}, your menu for today is:</h3>
-			<ul>
-			{{#meal}}
-				<li>
-					<b>{{ucFirst title}}</b><br />
-					Time: {{time}}<br />
-					Dishes: {{implode menu}}
-				</li>
-			{{/meal}}
-			</ul>',
-		'name' => '',
+		'data' => [
+			'jumps' => [
+				[
+					'personA' => 'brown fox',
+					'personB' => 'dog',
+				],
+				[
+					'personA' => 'crazy elk',
+					'personB' => 'brutal crocodile',
+				],
+				[
+					'personA' => 'smelly squirrel',
+					'personB' => 'gorgeous giraffe',
+				]
+			]
+		],
+		'template' => '<h3>Jumping around:</h3> {{#jumps}} {{> jumping}} <br /> {{/jumps}}',
+		'name' => 'massive_jump',
 	),
-	array(
-		'data' => [],
-		'template' => '',
-		'name' => '',
-	),
-	array(
-		'data' => [],
-		'template' => '',
-		'name' => '',
-	),
-	*/
 );
-?>
-
-<?/*
-<form>
-	<h3>Your order is:</h3>
-	<div class="container">
-		{{#if isNotEmpty basket}}
-			<table cellpadding="0" cellspacing="0">
-				<thead><tr><th>Name</th><th>Price</th><th>Quantity</th><th>Cost</th></tr></thead>
-				<tbody>
-				{{#basket}}
-				<tr>
-					<td>{{name}}</td>
-					<td>${{price}}</td>
-					<td>{{quantity}}</td>
-					<td>{{#if discount}}<s>{{/if}}${{getCost this}}{{#if discount}}</s> (with discount: ${{getDiscountCost this}}){{/if}}</td>
-					</tr>
-				{{/basket}}
-				</tbody>
-			</table>
-		{{else}}
-			<span class="red">Your basket is empty</span>
-		{{/if}}
-	</div>
-	Total price: <span class="green">${{totalPrice basket}}</span><br /><br />
-	Pay with:
-	<select>
-		{{#payment}}
-		<option value="{{value}}" {{#if default}} selected="selected"{{/if}}>{{title}}</option>
-		{{/payment}}
-	</select>
-</form>
-*/?>
-
-
-
-<?
-/*
-$data = [
-	'basket'=> [
-		[
-			'name'=> 'Fish&Chips',
-			'quantity'=> 3,
-			'price'=> 10
-		],
-		[
-			'name'=> 'Pair of socks',
-			'quantity'=> 5,
-			'price'=> 700
-		],
-		[
-			'name'=> 'Microwave owen',
-			'quantity'=> 1,
-			'price'=> 1500
-		],
-		[
-			'name'=> 'Mercedes 600',
-			'quantity'=> 1,
-			'price'=> 900000000,
-			'discount'=> 0.03
-		]
-	],
-	'payment'=> [
-		['title'=> 'Visa/Master Card', 'value'=> 'card'],
-		['title'=> 'Cash', 'value'=> 'cash', 'default'=> true]
-	]
-];
-*/
 
 \Horns::toggleDebugMode(false);
-
 foreach($tests as $test)
 {
-	\Horns\Util::debug($test['template']);
+	//\Horns\Util::debug($test['template']);
 	$parser = Horns::compile($test['template'], $test['name']);
 	foreach($helpers as $name => $fn)
 	{
 		$parser->registerHelper($name, $fn);
 	}
 
-	$parser->outputStructure();
+	//$parser->outputStructure();
 
-	print_r('----<br />');
 	print($parser->get($test['data']));
-	print_r('<br />----<br />');
+	print('<hr />');
 }
+
+?>
+
+</body>
+</html>
