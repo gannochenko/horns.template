46d679132ad64441d93089c950b0fb47038b2998
Sergey <awesome1888@gmail.com>
2016-07-21 00:27:15 +0200

    php tests work

--- tests/php/phpunit.php
+++ tests/php/phpunit.php
@@ -19,7 +19,36 @@ class MainTest extends PHPUnit_Framework_TestCase
 		foreach($tests as $test)
 		{
 			$parser = Horns::compile($test['template'], $test['name']);
-			$this->assertEquals(trim($test['result']), trim($parser->get($test['data'])), 'Test failed for '.$test['name']);
+			$this->assertSameNoWhitespace(trim($test['result']), trim($parser->get($test['data'])), 'Test failed for '.$test['name']);
 		}
+	}
+
+	public static function assertSameNoWhitespace($expected, $actual, $message = '')
+	{
+		self::assertThat($actual, new PHPUnit_Framework_Constraint_SameAsNoWhitespace(
+			$expected
+		), $message);
+	}
+}
+
+class PHPUnit_Framework_Constraint_SameAsNoWhitespace extends \PHPUnit_Framework_Constraint
+{
+	private $expected = '';
+
+	public function __construct($expected)
+	{
+		$this->expected = $expected;
+
+		parent::__construct();
+	}
+
+	public function matches($other)
+	{
+		return preg_replace('#\s#', '', (string) $other) == preg_replace('#\s#', '', $this->expected);
+	}
+
+	public function toString()
+	{
+		return ' equals to '.$this->expected;
 	}
 }
